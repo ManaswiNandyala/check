da31794775662cb186c8dbe377b341ef1b314159
ManaswiNandyala <nandyalamanaswi2002@gmail.com>
2024-05-06 12:28:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import cv2
+import streamlit as st
+import numpy as np
+
+# For webcam input:
+cap = cv2.VideoCapture(1)
+
+st.title("Obj Det webcam")
+frame_placeholder = st.empty()
+st.write(cap.isOpened())
